4879127baa9628cd3987f0e81250f575fba1e8eb
KolOmB0 <kkorostelev@inbox.ru>
2024-04-20 10:20:03 +0500

    object workingt

---
+++ Personal.py
@@ -0,0 +1,7 @@
+class Apparatchik:
+    def __init__(self, name, Fameli_name, surname, work_shift, datd_start):
+        self.name = name
+        self.Fameli_name = Fameli_name
+        self.surname = surname
+        self.work_shift = work_shift
+        self.datd_start = datd_start
